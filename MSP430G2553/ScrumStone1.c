1b159f0c9b9fc6bcd74f8ab3e8139849685046d0
Brian Harlow <harlowb6@students.rowan.edu>
2018-10-16 23:29:50 -0400

    V2 Push
    pushing version 2 which is a complete program

--- MSP430G2553/ScrumStone1.c
+++ MSP430G2553/ScrumStone1.c
@@ -7,19 +7,16 @@
 // Last Updated: 10 October 2018
 // Microcontroller: MSP430G2553
 
-// Program Description:
-
-// Execution Plan:
-//      1) Learn to use output modes for controlling pins with timer
-//      2) Confirm off-board works with program
-//      3) Learn to use microcontroller UART
-//      4) Allow program to take input from RXD and use as data for LED
-//      5) Allow program to send altered packets through TXD to other nodes
+// Program Description: This program is designed to use UART to chain different RGB LEDs together. Data can be transferred
+//                      between the different LEDs (nodes) so that an input will send data through the entire chain.
 
 
 /**
  * main.c
  */
+
+char packetSize = 0;            // Global variable definition for packet size
+char packetByte = 0;            // Global variable definition for current byte
 
 void cfgRGB(void){
     P1SEL |= BIT6;              // Sets P1.6 to output SET/RESET signal from Output Mode 3
@@ -27,45 +24,43 @@ void cfgRGB(void){
 
     P1DIR |= BIT6;              // Sets P1.6 to output SET/RESET signal from Output Mode 3
 
-    P2SEL |= (BIT1 + BIT4);     //
-    P2SEL2 &= ~(BIT1 + BIT4);   //
+    P2SEL |= (BIT1 + BIT4);     // Sets P2.1 and P2.4 to output SET/RESET signal from Output Mode 3
+    P2SEL2 &= ~(BIT1 + BIT4);   // Sets P2.1 and P2.4 to output SET/RESET signal from Output Mode 3
 
-    P2DIR |= (BIT1 + BIT4);     //
-}
-
-void cfgButton(void){
-    P1SEL &= ~BIT3;             // Sets P1.3 to GPIO
-    P1SEL2 &= ~BIT3;            // Sets P1.3 to GPIO
-
-    P1DIR &= ~BIT3;             // Sets P1.3 to input
-
-    // Pull-up Resistor
-    P1REN |= BIT3;              // Enable pull-up/pull-down resistor
-    P1OUT |= BIT3;              // Pull-Up/Pull-Down resistor set to pull-up
-
-    // Interrupt Setup
-    P1IE |= BIT3;               // Enable interrupt from P1.3
-    P1IES |= BIT3;              // Sets interrupt to trigger on falling edge
-    P1IFG &= ~BIT3;             // Clears the interrupt flag for P1.3
+    P2DIR |= (BIT1 + BIT4);     // Sets P2.1 and P2.4 to output SET/RESET signal from OutPut Mode 3
 }
 
 void cfgTimerA0(void){
-    TA0CTL = TASSEL_2 + MC_1 + ID_2 + TACLR + TAIE; // SMCLK selected in Up Mode, divided by 4, timer cleared, and timer interrupts enabled
+    TA0CTL = TASSEL_2 + MC_1 + ID_2 + TACLR;    // SMCLK selected in Up Mode, divided by 4, timer cleared
 
-    TAOCCR0 = 255;              // Sets CCR0 to 255
+    TA0CCR0 = 255;              // Sets CCR0 to 255
     TA0CCR1 = 0;                // Sets CCR1 to 0, controls RED in RGB
 
-    TA0CCTL1 = OUTMOD_3;
+    TA0CCTL1 = OUTMOD_3;        // Enables Output Mode 3 for TA0CCR1
 }
 
 void cfgTimerA1(void){
-    TA1CTL = TASSEL_2 + MC_1 + ID_2 + TACLR + TAIE;
+    TA1CTL = TASSEL_2 + MC_1 + ID_2 + TACLR;    // SMCLK selected in Up Mode, divided by 4, timer cleared
 
     TA1CCR0 = 255;              // Sets CCR0 to 255
     TA1CCR1 = 0;                // Sets CCR1 to 0, controls GREEN in RGB
     TA1CCR2 = 0;                // Sets CCR2 to 0, controls BLUE in RGB
 
-    TA1CCTL1 = OUTMOD_3;
+    TA1CCTL1 = OUTMOD_3;        // Enables Output Mode 3 for TA1CCR1
+    TA1CCTL2 = OUTMOD_3;        // Enables Output Mode 3 for TA1CCR2
+}
+
+void cfgUART(void){
+    P1SEL |= BIT1 + BIT2;       // Sets P1.1 and P1.2 to RXD and TXD, respectively
+    P1SEL2 |= BIT1 + BIT2;      // Sets P1.1 and P1.2 to RXD and TXD, respectively
+
+    UCA0CTL1 |= UCSSEL_2;       // CLK = SMCLK
+    UCA0BR0 = 109;              // Sets baud rate to 9600
+    UCA0BR1 = 0;                // Sets baud rate to 9600
+    UCA0MCTL = UCBRS_2;         // Sets baud rate to 9600
+
+    UCA0CTL1 &= ~ UCSWRST;      // Disable UART reset
+    UC0IE |= UCA0RXIE;          // Enable UART_RXD interrupts
 }
 
 int main(void)
@@ -73,18 +68,44 @@ int main(void)
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
 	cfgRGB();       // Configure pins used for RGB
-	cfgButton();    // Configure Button input
 	cfgTimerA0();   // Configure Timer A0
 	cfgTimerA1();   // Configure Timer A1
+	cfgUART();      // Configure UART
 
-	_BIS_SR(LPM0_bits + GIE);   // Enter LPM0 and enable global interrupts
-	return 0;
+	_BIS_SR(LPM0_bits + GIE);   // Forces microcontroller into LPM0 and enables global interrupts
 }
 
-// Button Interrupt Service Routine
-#pragma vector = PORT1_VECTOR
-__interrupt void Port_1(void) {
 
+#pragma vector=USCIAB0RX_VECTOR
+__interrupt void USCI0RX_ISR(void)
+{
+   char data = UCA0RXBUF;           // Stores first byte of received packet into data char
+
+   if (packetByte == 0){            // Initializes when data is being received
+       packetByte = data;           // Stores the value of the current byte in the packet
+       packetSize = data;           // Stores the number of bytes in the packet received
+
+       if (data >= 8){              // Check condition for if there is sufficient data to send to next node in chain
+           UCA0TXBUF = data - 3;    // Sends expected packet size to next node in chain
+       }
+   }
+   else if (packetSize - packetByte == 1){  // Check condition for if current data is for red
+       TA0CCR1 = data;                      // Stores data into TA0CCR1 to control red
+   }
+   else if (packetSize - packetByte == 2){  // Check condition for if current data is for green
+       TA1CCR1 = data;                      // Stores data into TA1CCR1 to control green
+   }
+   else if (packetSize - packetByte == 3){  // Check condition for if current data is for blue
+       TA1CCR2 = data;                      // Stores data into TA1CCR2 to control blue
+   }
+   else {
+       if (packetSize >= 8){
+           UCA0TXBUF = data;        // Transmits the byte to the next node in the chain
+       }
+   }
+
+   packetByte -= 1;     // Decrements counter to represent the next byte in the packet
 }
+
 
 
